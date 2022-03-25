2144f5326d1a8e70540df2283fe4aee17a82dbf4
Tatvesh <tattu24112000@gmail.com>
2022-03-25 16:34:08 +0530

    first commit

---
+++ src/MainMint.js
@@ -0,0 +1,120 @@
+import {useState} from 'react';
+import {Box, Button, Flex, Input, Text} from '@chakra-ui/react';
+
+import {ethers, BigNumber} from 'ethers';
+
+import roboPunksNFT from './RoboPunksNFT.json'
+
+const roboPunksNFTAddress = "0xBA4687c5D8eb7CA58De8D5831eF24b663bDDb190"
+
+const MainMint = ( {accounts , setAccounts}) => {
+    const [mintAmount, setMintAmount] = useState(1);
+
+    const isConnected = Boolean(accounts[0])
+
+    async function handleMint() {
+        if(window.ethereum){
+            const provider = new ethers.providers.Web3Provider(window.ethereum);
+            const signer = provider.getSigner();
+            const contract = new ethers.Contract(
+                roboPunksNFTAddress,
+                roboPunksNFT.abi,
+                signer
+            );
+            try {
+                const response = await contract.mint(BigNumber.from(mintAmount), {value:ethers.utils.parseEther((0.02 * mintAmount).toString())});
+                console.log('response', response);
+            } catch (error) {
+                console.log("Error", error);
+            }
+        }
+    }
+
+    const handleDecrement = () => {
+        if(mintAmount <= 1) return;
+        setMintAmount(mintAmount-1);
+    }
+
+    const handleIncrement = () => {
+        if(mintAmount >= 3) return;
+        setMintAmount(mintAmount + 1);
+    }
+
+    return(
+        <Flex justify='center' align='center' height='100vh' paddingBottom='150px'>
+        <Box width='520px'>
+            <div>
+        <Text fontsize='40px' textShadow='0 5px #000000'>RoboPunks</Text>
+        <Text
+            fontsize='30px'
+            letterSpacing='-5.55'
+            fontfamily='VT323'
+            textShadow='0 2px 2px #000000'
+        >
+            It's 2078. Can the RoboPunks NFT save humans from the destructive rampant NFT speculation? Mint RoboPunks to find out.
+            </Text>
+        </div>
+        {
+            isConnected ? (
+                <div>
+                    <Flex justify='center' align='center'>
+                        <Button
+
+                            backgroundColor='#d65170'
+                            borderRadius='5px'
+                            boxShadow='0px 2px 2px 1px #0F0F0F'
+                            color='white'
+                            cursor='pointer'
+                            fontFamily='inherit'
+                            padding='15px'
+                            marginTop='10px'
+                        onClick={handleDecrement}>-</Button>
+
+                        <Input
+                         readOnly
+                         fontFamily='inherit'
+                         width='100px'
+                         height='40px'
+                         textAlign='center'
+                         paddingLeft='19px'
+                         marginTop='10px'
+                         
+                         type='number' value={mintAmount}/>
+
+                        <Button
+
+                            backgroundColor='#d65170'
+                            borderRadius='5px'
+                            boxShadow='0px 2px 2px 1px #0F0F0F'
+                            color='white'
+                            cursor='pointer'
+                            fontFamily='inherit'
+                            padding='15px'
+                            marginTop='10px'
+                            onClick={handleIncrement}>+</Button>
+                    </Flex>
+
+                    <Button
+
+                            backgroundColor='#d65170'
+                            borderRadius='5px'
+                            boxShadow='0px 2px 2px 1px #0F0F0F'
+                            color='white'
+                            cursor='pointer'
+                            fontFamily='inherit'
+                            padding='15px'
+                            marginTop='10px' 
+                            onClick={handleMint}>Mint Now</Button>
+                </div>
+            ):
+            (
+                <p>You are not connected yet, you must be connected to Mint.</p>
+            )
+        }
+        </Box>
+        </Flex>
+    )
+
+}
+
+export default MainMint;
---
+++ src/Navbar.js
@@ -0,0 +1,63 @@
+import {Box, Button, Flex, Image, Link, Spacer} from '@chakra-ui/react';
+import Facebook from './assets/social-media-icons/facebook_32x32.png'
+import Email from './assets/social-media-icons/email_32x32.png'
+import Twitter from './assets/social-media-icons/twitter_32x32.png'
+import React from 'react'
+
+const Navbar = ({accounts , setAccounts}) => {
+  
+    
+  const isConnected = Boolean(accounts[0]);
+  
+  async function connectAccount(){
+      if(window.ethereum){
+          const accounts = await window.ethereum.request({method: "eth_requestAccounts",})
+          setAccounts(accounts)
+      }
+  }
+  return (
+    <Flex justify='space-between' align='center' padding='30px'>
+        {/* {Left} */}
+        <Flex justify='space-around' width='40%' padding='0 75px'>
+            <Link href='https://www.facebook.com'>
+                <Image src={Facebook} boxSize='42px' margin='0 15px' />
+            </Link>
+            <Link href='https://www.Twitter.com'>
+                <Image src={Twitter} boxSize='42px' margin='0 15px' />
+            </Link>
+            <Link href='https://www.gmmail.com'>
+                <Image src={Email} boxSize='42px' margin='0 15px' />
+            </Link>
+        </Flex>
+
+
+        {/* Right side */}
+        <Flex justify='space-around' align='center' width='40%' padding='30px'>
+        <Box margin='0 15px'>About</Box>
+        <Spacer/>
+        <Box margin='0 15px'>Mint</Box>
+        <Spacer/>
+        <Box margin='0 15px'>Team</Box>
+        <Spacer/>
+
+
+        {/* Connect Button */}
+        {isConnected? (<Box margin='0 15px '>Connected</Box>): 
+        (<Button 
+            backgroundColor='#D65170'
+            borderRadius='5px'
+            boxshadow='0px 2px 2px 1px #0F0F0F'
+            color='white'
+            cursor='pointer'
+            fontFamily='inherit'
+            padding='15px'
+         onClick={connectAccount}>Connect Your Wallet</Button>)}
+        </Flex>
+
+
+
+    </Flex>
+  )
+}
+
+export default Navbar
